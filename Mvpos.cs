fd7be732946a79c9fb3fb308ca9522a5c195fc40
Byron Custodio <byron.custodio@gmail.com>
2024-03-02 20:51:09 -0800

    Initial commit

---
+++ Mvpos.cs
@@ -0,0 +1,11 @@
+﻿namespace MvposSDK;
+
+public class Mvpos
+{
+    private readonly HttpClient _httpClient;
+    
+    public Mvpos(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+}
