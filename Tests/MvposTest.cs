53ca7b610fc495ab06967e8740c5ebf349807dd8
Byron Custodio <byron.custodio@gmail.com>
2024-04-09 15:14:32 -0700

    Adding tests for services

---
+++ Tests/MvposTest.cs
@@ -0,0 +1,51 @@
+using System.Diagnostics;
+
+namespace MvposSDK.Tests;
+
+public class Tests
+{
+    private Mvpos? _mvpos;
+
+    private const string Email = "mirusakii@gmail.com";
+
+    private const string Password = "Sakura780";
+    
+    [SetUp]
+    public void Setup()
+    {
+        _mvpos = new Mvpos(new HttpClient());
+    }
+
+    [Test]
+    public async Task TestUser_Login()
+    {
+        Debug.Assert(_mvpos != null, nameof(_mvpos) + " != null");
+        await _mvpos.Users.Login(Email, Password);
+        
+        var a = await _mvpos.Vendors.Get(88896747);
+        var b = 1;
+    }
+
+    [Test]
+    public async Task TestUser_SetStoreLocation()
+    {
+        var location = Mvpos.StoreLocation.Guildford;
+        
+        Debug.Assert(_mvpos != null, nameof(_mvpos) + " != null");
+        await TestUser_Login();
+
+        await _mvpos.Users.SetStoreLocation(location);
+    }
+    
+    [Test]
+    public async Task TestSaleItem_List()
+    {
+        var from = new DateTime(2024, 1, 1);
+        var to = new DateTime(2024, 1, 31);
+        
+        Debug.Assert(_mvpos != null, nameof(_mvpos) + " != null");
+        await TestUser_SetStoreLocation();
+
+        await Assert.ThatAsync(async () => (await _mvpos.SaleItems.List(from, to)).Items.Count, Is.GreaterThan(0));
+    }
+}
